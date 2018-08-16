bfac04fe67a139e38bd37066a00b203bb3dbfbb1
mayoco <ma_yo233@sina.cn>
2018-08-16 16:51:16 +0800

    Simple script to start host or join network
    Maybe useful in future. Simple script can use in tap button and start host or join network.

---
+++ NetworkController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+public class NetworkController : MonoBehaviour
+{
+    NetworkManager netManager;
+    NetworkClient host;
+    void Start()
+    {
+        netManager = GetComponent<NetworkManager>();
+    }
+    public void StartHost()
+    {
+        Debug.Log("Starting host");
+        netManager.StartHost();
+        Debug.Log("Started host");
+    }
+    public void StartClient()
+    {
+        NetworkClient client = netManager.StartClient();
+    }
+    public void OnDestroy()
+    {
+        netManager.StopClient();
+        netManager.StopHost();
+    }
+}

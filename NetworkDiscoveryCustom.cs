d1cea2733369a849213ec9562162df778fc25aa4
mayoco <ma_yo233@sina.cn>
2018-08-16 13:29:27 +0800

    Add my unet solution for hololens
    Attach those scripts to unetsharemanager gameobject then add client prefab in spawn info of networkmanagercustom，all done！

---
+++ NetworkDiscoveryCustom.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class NetworkDiscoveryCustom : NetworkDiscovery
+{
+    public override void OnReceivedBroadcast(string fromAddress, string data)
+    {
+        StopBroadcast();
+        NetworkManagerCustom.singleton.networkAddress = fromAddress;
+        NetworkManagerCustom.singleton.StartClient();
+    }
+}
---
+++ NetworkManagerCustom.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+
+public class NetworkManagerCustom : NetworkManager
+{
+    void Start()//两秒后自动运行，开始连接网络
+    {
+        Invoke("LanGame", 2);
+    }
+
+    public void LanGame()
+    {
+        singleton.StartCoroutine((singleton as NetworkManagerCustom).DiscoverNetwork());
+
+    }
+    
+    public IEnumerator DiscoverNetwork()
+    {
+        NetworkTransport.Init();
+        //监听其它的服务器
+        NetworkDiscoveryCustom discovery = GetComponent<NetworkDiscoveryCustom>();
+        discovery.Initialize();
+        discovery.StartAsClient();
+
+
+        //等待2秒，等监听结果
+
+        yield return new WaitForSeconds(2);
+
+        //没有找到局域网服务器就建立服务器
+
+        if (discovery.running)
+
+        {
+            discovery.StopBroadcast();
+            yield return new WaitForSeconds(0.5f);
+            discovery.StartAsServer();
+            StartHost();
+        }
+
+        
+    }
+}
